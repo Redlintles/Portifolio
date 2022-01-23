09c9c0d88714a0764662b8ad03e81eb2469e5137
João Paulo <treelands54@gmail.com>
2022-01-23 13:50:33 -0300

    Adicionado o terceiro projeto do curso de tailwind(HDC);Feito a olho

---
+++ Tailwind CSS/clone_amazon_2/tailwind.config.js
@@ -0,0 +1,42 @@
+const {colors} = require("tailwindcss/defaultTheme")
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'arial': "Arial, sans-serif"
+      },
+      colors: {
+        "darkblue": {
+          "500":"#485769",
+          "600":"#37475A",
+          "700":"#232F3E",
+          "800":"#191E26",
+          "900":"#131A22",
+        },
+        "gold": {
+          "100":"#FEBD69",
+          "200":"#FAA634",
+        },
+        "orange": {
+          ...colors.orange,
+          "600": "#E47911",
+        },
+        "yellow": {
+          ...colors.yellow,
+          "300": "#F0C140"
+        }
+      },
+      spacing: {
+        "7": "1.75rem",
+        "75px": "75px",
+        "210px": "210px",
+        "300px": "300px",
+        "150px": "150px",
+        "240px": "240px",
+      }
+    },
+  },
+  plugins: [],
+}
