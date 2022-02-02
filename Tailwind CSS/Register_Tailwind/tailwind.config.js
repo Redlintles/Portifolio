7d687ee3ea3d7515c7d41b4a2e1e668c820e83df
João Paulo <treelands54@gmail.com>
2022-02-02 20:24:51 -0300

    Adicionado um remake do Projeto Register; Feito Com Tailwind

---
+++ Tailwind CSS/Register_Tailwind/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "main-white": "#F8F8F8",
+        "main-purple": "#6C63FF"
+      }
+    },
+  },
+  plugins: [],
+}
