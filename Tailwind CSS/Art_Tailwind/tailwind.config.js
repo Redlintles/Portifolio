1e9069bc297bfe3b180eac25171e28499a034629
João Paulo <treelands54@gmail.com>
2022-02-02 20:23:26 -0300

    Adicionado um remake do projeto Art;Feito com Tailwind

---
+++ Tailwind CSS/Art_Tailwind/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "main-dark": "#25282E",
+        "main-dark-hover": "#333",
+        "secondary-color": "#555",
+        "bege": "#C1B696",
+      }
+    },
+  },
+  plugins: [],
+}
